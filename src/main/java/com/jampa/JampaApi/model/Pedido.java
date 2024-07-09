d21efdd117a489d7517bdd52e4c069a544fe7e9e
Plinio <mcplinio.15@gmail.com>
2024-07-09 14:53:19 -0500

    Relacion de tablas

--- src/main/java/com/jampa/JampaApi/model/Pedido.java
+++ src/main/java/com/jampa/JampaApi/model/Pedido.java
@@ -1,7 +1,17 @@
 package com.jampa.JampaApi.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "pedidos")
 public class Pedido {
@@ -9,6 +19,10 @@ public class Pedido {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id_pedido;
   private int cantidad;
+
+  @OneToMany
+  @JoinColumn(name = "id_usuario")
+  private List<Usuario> usuario;
   /* Pendiente relaciones con
   Long id_usuario;
   Long id_producto;
